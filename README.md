1011d49d3224e540585e33cb61c7e3bad05db5c3
Ector <p22.jose@webmotors.com.br>
2023-04-14 21:48:21 -0300

    Setuping jest and RTL and install styled components and yours types

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Generic react boilerplate without CRA
 Run ```yarn start``` for execute the project.
+
+-----
+
+# Run unit tests
+-  ``` yarn test```
+
+- ``` yarn test:watch```
+
+- ``` yarn test:coverage```
---
+++ src/components/PageTitle/main.test.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+
+import PageTitle from './main';
+
+describe('PageTitle unit test', () => {
+    it('should be able to show the page title', () => {
+        render(<PageTitle />);
+        const title = screen.getByText(/Title/i);
+        expect(title).toBeInTheDocument();
+    });
+});
